0f3f2d2498964102fe16613fc5dd2a48c3d9270f
Aviad houri <aviadhuri3@gmail.com>
2020-08-04 12:54:10 +0300

    click

--- src/components/CallbackMsg/CallbackMsg.tsx
+++ src/components/CallbackMsg/CallbackMsg.tsx
@@ -17,12 +17,12 @@ function CallbackMsg(props: ICallbackMsgProps) {
 		<Paper elevation={3} style={{ padding: "10px 0px" }} >
 			<Grid style={{ padding: "10px" }} item>
 				<Typography variant="h4">המשתמש נוצר בהצלחה!</Typography>
-				<Typography variant="h6" style={{fontWeight: "bold"}}>שם המשתמש האישי שלך הינו:</Typography>
+				<Typography variant="h6" style={{ fontWeight: "bold" }}>שם המשתמש האישי שלך הינו:</Typography>
 				<Typography dir="ltr" className="english-font" style={{ color: "#2196f3" }}>{account}</Typography>
-				<Typography variant="h6" style={{fontWeight: "bold", marginTop: "10px"}}>סיסמה:</Typography>
+				<Typography variant="h6" style={{ fontWeight: "bold", marginTop: "10px" }}>סיסמה:</Typography>
 				<Typography>כפי שנקבעה בתהליך הרישום</Typography>
-				<Typography style={{fontWeight: "bold", marginTop: "10px"}}>ניתן לחזור לאתר ולבצע התחברות באמצעות המשתמש.</Typography>
-				
+				<Typography style={{ fontWeight: "bold", marginTop: "10px" }}>ניתן לחזור לאתר ולבצע התחברות באמצעות המשתמש.</Typography>
+
 			</Grid>
 			<Grid container item justify="center" alignItems="center" md={12} >
 				<Grid item xs={3} sm={8} md={6}>
