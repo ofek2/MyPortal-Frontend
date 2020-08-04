34ba7228b37b6a296c0d552b0439f4f1688f699d
ofek <ofekaz24@gmail.com>
2020-08-04 12:07:22 +0300

    update

--- src/model/data/Configuration.ts
+++ src/model/data/Configuration.ts
@@ -1,6 +1,6 @@
 import * as _ from 'lodash';
 import * as config from '../../configuration.json';
-console.log(process.env.WEBSITE_SITE_NAME)
+
 const defaultConfig = config.development;
 const environment = process.env.REACT_APP_ENVIRONMENT || 'development';
 const envConf = config[environment];
